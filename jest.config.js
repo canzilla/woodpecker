a9793bce706242bae86e361684fb7bdb7c58810c
canzilla <62693556+canzilla@users.noreply.github.com>
2020-03-28 14:09:57 +0300

    Add files via upload

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   setupFilesAfterEnv: ['./test/testSetup.js'],
+  moduleFileExtensions: ['js', 'json', 'node'],
+  collectCoverage: true,
+  clearMocks: true,
+  coverageDirectory: "coverage",
 };
