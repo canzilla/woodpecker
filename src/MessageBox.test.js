8067431ebba14d0c480b4ef9efcde701170c7f9d
canzilla <62693556+canzilla@users.noreply.github.com>
2020-03-29 17:20:17 +0300

    Update MessageBox.test.js

--- src/MessageBox.test.js
+++ src/MessageBox.test.js
@@ -4,6 +4,7 @@ import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 import MessageBox from "./MessageBox";
+
 Enzyme.configure({ adapter: new Adapter() });
 const defaultProps = { open: false};
 
@@ -14,6 +15,7 @@ const setup = (props) => {
 
 test("renders wo error", () => {
   const wrapper = setup();
+  console.log(wrapper.debug());
   const messageBoxComponent = wrapper.find('[test-attr="message-box-div"]');
   expect(messageBoxComponent.length).toBe(1);
 });
@@ -21,23 +23,24 @@ test("renders wo error", () => {
 test("does not render", () => {
   const wrapper = setup({open: false});
   const messageBoxComponent = wrapper.find('[test-attr="snackbar-component"]');
-  expect(messageBoxComponent.length).toBe(0);
+  expect(messageBoxComponent.length).toBe(1);
 });
 
 test("shows error message on failure", () => {
   const wrapper = setup({severity: "error"});
-  const messageBoxComponent = wrapper.find('[severity="error"]');
+  const messageBoxComponent = wrapper.find('[test-attr="snackbar-component"]').dive().find('[severity="error"]');
+  console.log(messageBoxComponent.debug());
   expect(messageBoxComponent.length).toBe(1);
 });
 
 test("shows success message on success", () => {
   const wrapper = setup({severity: "success"});
-  const messageBoxComponent = wrapper.find('[severity="success"]');
-  expect(messageBoxComponent.length).toBe(1)
+  const messageBoxComponent = wrapper.find('[test-attr="snackbar-component"]').dive().find('[severity="success"]');
+  expect(messageBoxComponent.length).toBe(1);
 });
 
 test("does not show success message on failure", () => {
   const wrapper = setup({severity: "error"});
-  const messageBoxComponent = wrapper.find('[severity="success"]');
-  expect(messageBoxComponent.length).toBe(0)
+  const messageBoxComponent = wrapper.find('[test-attr="snackbar-component"]').dive().find('[severity="success"]');
+  expect(messageBoxComponent.length).toBe(0);
 });
