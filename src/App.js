c6ba2e917d4cc5b3e35003ff74f7517df5b5a9db
canzilla <62693556+canzilla@users.noreply.github.com>
2020-03-31 15:25:27 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import MessageBox from './MessageBox';
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hey There!</h1>
       <TextBoxes />
       <MessageBox />
     </div>
