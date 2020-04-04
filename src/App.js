e2c0ce27843f166baec13a7427604f1d68b08425
canzilla <62693556+canzilla@users.noreply.github.com>
2020-04-04 10:03:54 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import TextBoxes from './TextBoxes';
 import MessageBox from './MessageBox';
 
 export default function App() {
+  const objectFromServer = {
+    "thizz" : "this",
+    "shall" : "shall",
+    "pass" : "pass",
+    "too" : "too"
+  }
+  const openCloseState = false;
   return (
     <div className="App">
-      <h1>Hey There!</h1>
-      <TextBoxes />
-      <MessageBox open={false} severity="error" />
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <TextBoxes objectToDisplay={objectFromServer}/>
+      <MessageBox open={openCloseState} openseverity="error" />
     </div>
   );
 }
