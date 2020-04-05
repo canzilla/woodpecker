e13dac30066c2862533edcaa9297eca9f82e2ab1
unknown <onerbasak@gmail.com>
2020-04-05 12:28:32 +0300

    fixes

--- src/TextBoxes.js
+++ src/TextBoxes.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
-function TextBoxes (props) {//todo bunu sonra state'ten alacagız
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
+
+function TextBoxes (props) {//todo bunu sonra state'ten alacagız
+  const classes = useStyles();
   const textBox = Object.keys(props.objectToDisplay).map((attr, index)  => 
-    <TextField test-attr="text-box-span" defaultValue={props.objectToDisplay[attr]} key={index} />
+    <TextField 
+      test-attr="text-box-span"
+      defaultValue={props.objectToDisplay[attr]}
+      key={index} 
+      variant="outlined"
+      />
   )
 
   return (
-    <div test-attr="text-boxes-div">
+    <div className={classes.root} test-attr="text-boxes-div">
     {textBox}
     </ div>
   );
