dc05a087d0069ef1f8503ce0ae040f19e8ff37d4
mrgirzmet <samtebirah@gmail.com>
2020-04-18 19:10:23 +0300

    mini fix

--- base-test-mobx/src/components/Submit.js
+++ base-test-mobx/src/components/Submit.js
@@ -1,11 +1,15 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import {observer} from 'mobx-react-lite';
+import {inject} from 'mobx-react';
 
-export default function Submit(){
+const Submit = inject('Store')(observer(({Store: 
+  { setOpenCloseState }}) => {
     return(
       <div test-attr="test-submit-div">
-        <Button test-attr="test-submit-button" variant="contained">Save</Button>
+        <Button test-attr="test-submit-button" variant="contained" onClick={()=>{setOpenCloseState(true);}}>Save</Button>
       </div>
     );
-}
+}));
+export default Submit;
